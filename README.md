9ccdbe04c2e82478dd3f62455ad3016e56c10d75
Nathan Moore <moore.2289@buckeyemail.osu.edu>
2024-02-18 13:13:14 -0500

    Created io.py

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Test repo
 
 A new repo to test creating one in vscode rather than on GitHub.
+And to run through basics of python
---
+++ io.py
@@ -0,0 +1,9 @@
+# printing
+# automatically adds newline
+print("hello world")
+print (4.5)     # can print literals
+print(4.5, "four point five") # automatically adds a space b/w
+
+# choose how to end the print statement - default is end='\n'
+print("hello", end='|')
+print("world")
